e57925209af583030624637ee398b427dbf32560
rg <raska.gergely@gmail.com>
2021-04-08 12:45:48 +0200

    Add Logger + Poem

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,23 @@ import org.apache.logging.log4j.core.appender.ConsoleAppender;
 
 public class Main {
 
+    static final Logger easterLogger = LogManager.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+
+        /* TODO
+         *
+         *  Get args -> i, log poem i times;
+         *
+         * */
+        String[] poem = new String[]
+                {"Zöld erdőben jártam.",
+                        "Kék ibolyát láttam.",
+                        "El akart hervadni.",
+                        "Szabad-e locsolni?"};
+
+
+        easterLogger.trace("Zöld erdőben jártam.");
+    }
 
 }
